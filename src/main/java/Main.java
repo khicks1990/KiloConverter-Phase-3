94044b7d7968a0152d6478411d5759bbe345e3f4
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 06:58:17 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Main {
+       /**
+          main method
+       */
+
+       public static void main(String[] args)
+       {
+          new KiloConverter();
+       }
+}
